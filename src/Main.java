88f606014c4b4cf3901c2ec650fd11bedb130ba2
Borsuki23 <arturgnat2009@gmail.com>
2025-02-20 15:22:01 +0200

    dz8

--- src/Main.java
+++ src/Main.java
@@ -1,120 +1,75 @@
-class Animal {
+class Employee {
     private String name;
     private int age;
-    private String breed;
-    private int price;
+    private double salary;
 
-    public Animal() {}
-
-    public Animal(String name, int age, String breed, int price) {
+    public Employee(String name, int age, double salary) {
         this.name = name;
         this.age = age;
-        this.breed = breed;
-        this.price = price;
+        this.salary = salary;
     }
 
-    public String getName() { return name; }
-    public int getAge() { return age; }
-    public String getBreed() { return breed; }
-    public int getPrice() { return price; }
-
-    public void displayInfo() {
-        System.out.println("Ім'я: " + name + ", Вік: " + age + ", Порода: " + breed + ", Ціна: " + price);
-    }
-}
-
-class ZooShop {
-    private Animal[] animals;
-    private int count;
-
-    public ZooShop(int size) {
-        animals = new Animal[size];
-        count = 0;
+    public double getSalary() {
+        return salary;
     }
 
-    public void addAnimal(Animal animal) {
-        if (count < animals.length) {
-            animals[count] = animal;
-            count++;
-            System.out.println(animal.getName() + " додано до магазину.");
-        } else {
-            System.out.println("Магазин заповнений!");
-        }
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
 
-    public void removeAnimal(String name) {
-        for (int i = 0; i < count; i++) {
-            if (animals[i].getName().equals(name)) {
-                System.out.println(name + " продано!");
-                animals[i] = animals[count - 1];
-                animals[count - 1] = null;
-                count--;
-                return;
-            }
-        }
-        System.out.println("Тварину не знайдено.");
-    }
-
-    public void displayAnimals() {
-        System.out.println("Список тварин у магазині:");
-        for (int i = 0; i < count; i++) {
-            animals[i].displayInfo();
-        }
-    }
-
-    public Animal findAnimal(String name) {
-        for (int i = 0; i < count; i++) {
-            if (animals[i].getName().equals(name)) {
-                return animals[i];
-            }
-        }
-        return null;
-    }
-}
-
-class Customer {
-    private String name;
-    private int money;
-
-    public Customer(String name, int money) {
-        this.name = name;
-        this.money = money;
-    }
-
-    public void buyAnimal(ZooShop shop, String animalName) {
-        Animal animal = shop.findAnimal(animalName);
-        if (animal != null && money >= animal.getPrice()) {
-            money -= animal.getPrice();
-            shop.removeAnimal(animalName);
-            System.out.println(name + " купив " + animalName + " за " + animal.getPrice() + " грн.");
-        } else {
-            System.out.println("Не вдалося купити " + animalName + ". Можливо, не вистачає грошей або тварина відсутня.");
+    public void increaseSalary(double amount) {
+        if (amount > 0) {
+            this.salary += amount;
         }
     }
 
     public void displayInfo() {
-        System.out.println("Ім'я покупця: " + name + ", Гроші: " + money + " грн");
+        System.out.println("Имя: " + name + ", Возраст: " + age + ", Зарплата: " + salary);
+    }
+}
+
+class Manager extends Employee {
+    private double bonus;
+
+    public Manager(String name, int age, double salary, double bonus) {
+        super(name, age, salary);
+        this.bonus = bonus;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Бонус: " + bonus);
+    }
+}
+
+class Developer extends Employee {
+    private String programmingLanguage;
+
+    public Developer(String name, int age, double salary, String programmingLanguage) {
+        super(name, age, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Язык программирования: " + programmingLanguage);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        ZooShop shop = new ZooShop(10);
+        Manager manager = new Manager("Алексей", 40, 80000, 10000);
+        Developer developer = new Developer("Иван", 30, 60000, "Java");
 
-        Animal cat = new Animal("Барсик", 3, "Сибірська", 500);
-        Animal dog = new Animal("Рекс", 5, "Німецька вівчарка", 1000);
+        System.out.println("До повышения зарплаты:");
+        manager.displayInfo();
+        developer.displayInfo();
 
-        shop.addAnimal(cat);
-        shop.addAnimal(dog);
+        manager.increaseSalary(5000);
 
-        shop.displayAnimals();
-
-        Customer customer = new Customer("Іван", 1200);
-        customer.displayInfo();
-
-        customer.buyAnimal(shop, "Рекс");
-        customer.displayInfo();
-
-        shop.displayAnimals();
+        System.out.println("После повышения зарплаты менеджеру:");
+        manager.displayInfo();
     }
 }
