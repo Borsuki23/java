3c11c9f5cbe39259540872bf07cd8aed02442c72
Borsuki23 <arturgnat2009@gmail.com>
2025-03-10 21:41:38 +0200

    dz12

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
-
-    public static boolean isPalindrome(String s) {
-        return s.equals(new StringBuilder(s).reverse().toString());
-    }
-
-    public static int countVowels(String s) {
-        return (int) s.chars().filter(c -> "aeiouAEIOU".indexOf(c) != -1).count();
-    }
-
-    public static String reverseString(String s) {
-        return new StringBuilder(s).reverse().toString();
-    }
-
-    public static boolean isEmptyOrWhitespace(String s) {
-        return s.trim().isEmpty();
-    }
-
-    public static int countLetters(String s) {
-        return (int) s.chars().filter(Character::isLetter).count();
-    }
-
-    public static int countWords(String sentence) {
-        return sentence.trim().isEmpty() ? 0 : sentence.split("\\s+").length;
-    }
-
-    public static String replaceDigits(String s) {
-        return s.replaceAll("\\d", "*");
-    }
-
-    public static boolean areAnagrams(String s1, String s2) {
-        char[] arr1 = s1.toLowerCase().toCharArray();
-        char[] arr2 = s2.toLowerCase().toCharArray();
-        java.util.Arrays.sort(arr1);
-        java.util.Arrays.sort(arr2);
-        return java.util.Arrays.equals(arr1, arr2);
-    }
-
-    public static String compressString(String s) {
-        StringBuilder compressed = new StringBuilder();
-        int count = 1;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                count++;
-            } else {
-                compressed.append(s.charAt(i - 1)).append(count);
-                count = 1;
-            }
-        }
-        compressed.append(s.charAt(s.length() - 1)).append(count);
-        return compressed.length() < s.length() ? compressed.toString() : s;
-    }
-
-    public static String longestCommonSubstring(String s1, String s2) {
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
-        int maxLength = 0, endIndex = 0;
-        for (int i = 1; i <= s1.length(); i++) {
-            for (int j = 1; j <= s2.length(); j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                    if (dp[i][j] > maxLength) {
-                        maxLength = dp[i][j];
-                        endIndex = i;
-                    }
-                }
-            }
-        }
-        return s1.substring(endIndex - maxLength, endIndex);
-    }
-
     public static void main(String[] args) {
-        System.out.println(isPalindrome("racecar"));
-        System.out.println(countVowels("hello"));
-        System.out.println(reverseString("hello"));
-        System.out.println(isEmptyOrWhitespace("   "));
-        System.out.println(countLetters("hello123"));
-        System.out.println(countWords("This is a simple sentence"));
-        System.out.println(replaceDigits("Hello123"));
-        System.out.println(areAnagrams("listen", "silent"));
-        System.out.println(compressString("aabcccccaaa"));
-        System.out.println(longestCommonSubstring("abcdef", "zbcdf"));
+        SwingUtilities.invokeLater(() -> new Main().createAndShowGUI());
+    }
+
+    private void createAndShowGUI() {
+        JFrame frame = new JFrame("Калькулятор");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 500);
+        frame.setLayout(new BorderLayout());
+
+        JTextField display = new JTextField();
+        display.setEditable(false);
+        display.setFont(new Font("Arial", Font.BOLD, 24));
+        display.setHorizontalAlignment(JTextField.RIGHT);
+        frame.add(display, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 4, 10, 10));
+        String[] buttons = {"7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", "C", "=", "+"};
+
+        for (String text : buttons) {
+            JButton button = new JButton(text);
+            button.setFont(new Font("Arial", Font.BOLD, 20));
+            button.setBackground(Color.LIGHT_GRAY);
+            button.setOpaque(true);
+            button.setBorderPainted(false);
+
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    button.setBackground(Color.ORANGE);
+                    Timer timer = new Timer(200, evt -> button.setBackground(Color.LIGHT_GRAY));
+                    timer.setRepeats(false);
+                    timer.start();
+                }
+            });
+
+            buttonPanel.add(button);
+        }
+
+        frame.add(buttonPanel, BorderLayout.CENTER);
+        frame.setVisible(true);
     }
 }
